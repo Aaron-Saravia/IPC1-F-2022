ea81e8648136ade02b0bba28bd1b1d99e9dbdf76
Aaron-Saravia <aaronsaravia2002@gmail.com>
2022-02-16 15:00:50 -0600

    Esto es una prueba de subida

---
+++ Hola mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,42 @@
+
+package hola.mundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("hola mundo");
+        System.out.println("wenas");
+      
+        
+        int num1 = 5;
+        int num2 = 6;
+        
+        if (num2 > num1) {
+        System.out.println("El num2 es mayor al num1");
+        
+        
+        int num3 = 6;
+        int num4 = 4;
+        
+        if (num3 < num4) {
+        System.out.println("El num 3 es mayor al num 4");
+        }  
+        else {
+            System.out.println("El num 3 es menor al num 4");
+        }
+        
+        {
+        int num1, num2, resultado;
+        
+        num1 = 4;
+        num2 = 6;
+        
+        resultado = num 1 + num 2;
+        System.out.println("El resultado es la suma de:" + resultado);
+        }
+    }
+        
+    }
+    
+}
